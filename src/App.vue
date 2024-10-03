25d97f367539238c9670114558537fe40041e11a
Daniel <danielbeloded@gmail.com>
2024-10-03 17:19:09 +0300

    uploading new project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/nav/Navbar.vue";
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
+<style></style>
---
+++ src/components/nav/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar__logo">
+      <div class="navbar__wrapper">
+        <img src="../../assets/weight.png" alt="logo" />
+        <div class="navbar__name">GymFit</div>
+      </div>
+    </div>
+    <ul class="navbar__list">
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/profile">Profile</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import "./nav.scss";
+export default {
+  name: "Navbar",
+};
+</script>
